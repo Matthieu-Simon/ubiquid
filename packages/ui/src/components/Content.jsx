3f93ab51a3f7fd3a634c572541dc97b81b279b90
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-07-13 16:51:09 +0200

    feat: Add Header & Content component and add style Button Header component

---
+++ packages/ui/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const Content = () => {
+  return (
+    <main className="w-[1340px] gap-8 m-auto">
+      <Header />
+    </main>
+  )
+}
+
+export default Content;
