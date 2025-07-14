bc8fcd1a30c287eacd67f7ed5072ba8a4dd96dc9
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-07-14 23:32:46 +0200

    feat: add createJob controller

---
+++ packages/api/src/features/jobs/controllers/createJob.js
@@ -0,0 +1,41 @@
+import { db } from "../../../../db/db.js";
+
+export const createJob = (req, res) => {
+  try {
+    const { jobType, remoteType, companyName, contractType, location, salary } = req.body;
+
+    if(!jobType || !companyName || !location || !contractType || !salary || !remoteType) {
+      return res.status(400).json({
+        message: "Tous les champs doivent être remplis !"
+      });
+    }
+
+    const maxId = db.data.jobs.length > 0
+    ? Math.max(...db.data.jobs.map(job => job.id)) : 0;
+
+    const newJob = {
+      id: maxId + 1,
+      jobType,
+      remoteType,
+      companyName,
+      location,
+      contractType,
+      salary: parseInt(salary),
+      createdAt: new Date().toISOString()
+    };
+
+    db.data.jobs.push(newJob);
+
+    db.write();
+
+    res.status(201).json({
+      message: "Annonce créée avec succés",
+      job: newJob
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la création de l'annonce",
+      error: error.message
+    });
+  }
+}
--- packages/api/src/features/jobs/router.js
+++ packages/api/src/features/jobs/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { list } from "./controllers/list";
+import { list } from "./controllers/list.js";
+import { createJob } from "./controllers/createJob.js";
 
 const JobsRouter = Router();
 
 JobsRouter.get("/list", list);
+JobsRouter.post("/", createJob);
 
 export default JobsRouter;
