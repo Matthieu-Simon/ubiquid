a54b4434a3c48e7d799a2fefa2644c6483d137f1
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-07-14 18:01:06 +0200

    feat: Add modal in FilterDropdown

--- packages/ui/src/components/FilterDropdown.jsx
+++ packages/ui/src/components/FilterDropdown.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Arrow from "../../public/arrow.svg";
 
-const FilterDropdown = ({ theme }) => {
+const FilterDropdown = ({ theme, options, selected = [], onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between items-center gap-1 w-[128px] h-[41px] rounded-[8px] px-3 py-2.5 bg-white shadow-md">
-      <span className="font-medium text-[#0E0E2C]">{theme}</span>
-      <div className="flex items-center justify-center bg-[#F1EEFC] w-5 h-5 rounded-full">
-        <img src={Arrow} alt="open" className="w-2.5 h-1.5" />
-      </div>
+    <div className="relative flex items-center justify-between w-[128px] h-[41px] rounded-[8px] px-3 py-2.5 bg-white shadow-md cursor-pointer">
+      <button 
+        className="flex items-center gap-8 text-[#7650E0] font-semibold" 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {theme}
+        <img src="/arrow.svg" alt="open" className="w-3.5 h-2" />
+      </button>
+      
+
+      {isOpen && (
+        <div className="absolute top-8 left-0 bg-white border rounded shadow-md p-2 z-10">
+          {options.map((option) => (
+            <label key={option} className="flex items-center gap-2 py-1">
+              <input
+                type="checkbox"
+                checked={selected.includes(option)}
+                onChange={() => onChange(option)}
+              />
+              <span>{option}</span>
+            </label>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- packages/ui/src/components/TagFilter.jsx
+++ packages/ui/src/components/TagFilter.jsx
@@ -1,11 +1,13 @@
 import Cross from "../../public/crossIcon.png";
 
-const TagFilter = ({ content }) => {
+const TagFilter = ({ content, onRemove }) => {
   return (
-    <div className="flex items-center w-[191px] h-[38px] text-[#0E0E2C] font-medium border border-[#7650E0] px-4 py-2 bg-white gap-6 rounded-[38px]">
+    <div className="flex justify-between items-center w-[191px] h-[38px] text-[#0E0E2C] font-medium border border-[#7650E0] px-4 py-2 bg-white gap-6 rounded-[38px]">
       {content}
       <div className="w-5 h-5 flex items-center justify-center bg-[#7650E0] rounded-full">
-        <img src={Cross} alt="close" className="w-2 h-2" />
+        <button className="cursor-pointer">
+          <img src={Cross} alt="close" onClick={onRemove} className="w-2 h-2" />
+        </button>
       </div>
     </div>
   )
