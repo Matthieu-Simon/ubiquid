00fb4521f61f770574b2cbae17dbe13b9b2bdb31
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-07-15 18:22:52 +0200

    feat: add PUT request with updateJob controller

--- packages/api/src/features/jobs/router.js
+++ packages/api/src/features/jobs/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { list } from "./controllers/list.js";
 import { createJob } from "./controllers/createJob.js";
+import { updateJob } from "./controllers/updateJob.js";
 
 const JobsRouter = Router();
 
 JobsRouter.get("/list", list);
 JobsRouter.post("/", createJob);
+JobsRouter.put("/:id", updateJob);
 
 export default JobsRouter;
